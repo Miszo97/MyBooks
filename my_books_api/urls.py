dd2d1d1af01f9604f89de64f3e404150fa2b658f
Miszo <kol478@gmail.com>
2021-03-24 19:27:08 +0100

    Refactor code

--- my_books_api/urls.py
+++ my_books_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from my_books_api.views import BooksView
+from my_books_api.views import BooksDetailView, BooksView
 
 app_name = 'my_books_api'
 
 
 urlpatterns = [
-    path('books', BooksView.as_view(), name='books')
+    path('books', BooksView.as_view(), name='books'),
+    path('books/<int:id>', BooksDetailView.as_view(), name='books'),
 ]
--- my_books_api/views.py
+++ my_books_api/views.py
@@ -1,4 +1,5 @@
-from django.http.response import Http404, JsonResponse
+from django.http.response import JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,10 +10,36 @@ from my_books_api.serializers import BookSerializer
 
 class BooksView(APIView):
     def get(self, request):
+        title = request.GET.get('title', None)
+        author = request.GET.get('author', None)
+        language = request.GET.get('language', None)
+        pub_date = request.GET.get('pub_date', None)
 
-        books = Book.objects.all()
+        if title is None and author is None and pub_date is None and language is None:
+            books = Book.objects.all()
+        else:
+            filter_query = {}
+
+            if title is not None:
+                filter_query['title__icontains'] = title
+
+            if author is not None:
+                filter_query['author__icontains'] = author
+
+            if language is not None:
+                filter_query['language'] = language
+
+            if pub_date is not None:
+                if '-' in pub_date:
+                    from_year, to_year = pub_date.split('-')
+                    filter_query['pub_date__lte'] = to_year
+                    filter_query['pub_date__gte'] = from_year
+                else:
+                    filter_query['pub_date'] = pub_date
+
+            books = Book.objects.filter(**filter_query)
+
         serializer = BookSerializer(books, many=True)
-
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, requset):
@@ -24,27 +51,24 @@ class BooksView(APIView):
 
 
 class BooksDetailView(APIView):
+
     def get(self, request, id):
-        try:
-            return Book.objects.get(id)
-        except Book.DoesNotExist:
-            raise Http404
+        book = get_object_or_404(Book, id=id)
+        serializer = BookSerializer(book)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, id):
-        try:
-            book = Book.objects.get(id)
-        except Book.DoesNotExist:
-            raise Http404
-
+        book = get_object_or_404(Book, id=id)
         serializer = BookSerializer(book)
         book.delete()
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, id):
-        book = self.get_object(id)
+        book = get_object_or_404(Book, id=id)
         serializer = BookSerializer(book, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(data=serializer.data, status=status.HTTP_201_CREATED)
+            return JsonResponse(data=serializer.data, status=status.HTTP_200_OK)
         return JsonResponse(data="wrong parameters", status=status.HTTP_400_BAD_REQUEST)
