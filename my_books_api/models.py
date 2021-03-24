5b814d8d667d9c019db7434d25ef40fe900fc919
Miszo <kol478@gmail.com>
2021-03-24 19:21:23 +0100

    Change model's field

--- my_books_api/models.py
+++ my_books_api/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.PositiveSmallIntegerField()
     isbn_number = models.CharField(max_length=15)
     page_count = models.IntegerField()
     thumbnail = models.URLField()
     language = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.title} -- {self.author} -- {self.pub_date}'
