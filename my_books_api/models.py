792da0efb3bda6628efc059d18ecaf4ede775004
Miszo <kol478@gmail.com>
2021-03-28 15:16:39 +0200

    Make the language field optional

--- my_books_api/models.py
+++ my_books_api/models.py
@@ -8,7 +8,7 @@ class Book(models.Model):
     isbn_number = models.CharField(max_length=15, blank=True)
     page_count = models.IntegerField(null=True)
     thumbnail = models.URLField(blank=True)
-    language = models.CharField(max_length=10)
+    language = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return f'{self.title} -- {self.author} -- {self.pub_date}'
